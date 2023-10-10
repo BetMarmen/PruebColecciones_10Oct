697b7ffbc34b05c41bd20037d7739cd7b082082f
Sistemas2-20 <Sistemas2-20@DESKTOP-9MHM02N>
2023-10-10 12:01:10 -0600

    Agregar archivos de proyecto.

---
+++ PruebColecciones/Program.cs
@@ -0,0 +1,37 @@
+﻿//Crear una coleccion generica de 5 frutas
+//mostrar la informacion de la coleccion
+//coleccion(elementos,tamaño)
+//buscar en la coleccion la fruta naranja e imprimir
+//insertar un indice una nueva fruta e imprimir
+//remover ls fruts insertada
+//remover la fruta con el indice 0
+//imprimir
+using System.Collections.ObjectModel;
+
+Collection<string> frutas = new Collection<string>();
+frutas.Add("Manzana");
+frutas.Add("Melon");
+frutas.Add("Wanabana");
+frutas.Add("Naranja");
+frutas.Add("Sandia");
+Display(frutas);
+Console.WriteLine(" {0} frutas:", frutas.Count);
+    
+Console.WriteLine("\nContains(\"Naranja\"): {0}",
+           frutas.Contains("Naranja"));
+frutas.Insert(3, "Kiwi");
+Display(frutas);
+frutas.Remove("Kiwi");
+Display(frutas);
+frutas.RemoveAt(0);
+Display(frutas);
+ void Display(Collection<string> frutas)
+{
+    Console.WriteLine();
+    foreach (var fruta in frutas)
+    {
+        Console.WriteLine(fruta);
+        
+    }
+    Console.WriteLine("-----------------------");
+}
---
+++ PruebaList/Program.cs
@@ -0,0 +1,39 @@
+﻿string[] colores = { "Magenta", "Rojo", "Blanco", "Azul", "Cyan" };
+string[] eliminarColores = { "Rojo", "Blanco", "Azul" };
+List<string> lista = new List<string>();
+Console.WriteLine($"capacidad={lista.Capacity}");
+foreach (string color in colores)
+    lista.Add(color);
+List<string> eliminarLista = new List<string>(eliminarColores);
+MostrarInformacion(lista);
+EliminarColores(lista, eliminarLista);
+MostrarInformacion(lista);
+
+void EliminarColores(List<string> lista, List<string> eliminarLista)
+{
+    foreach(var item in eliminarLista)
+    {
+        lista.Remove(item);
+    }
+}
+
+void MostrarInformacion(List<string>lista)
+{
+    foreach(var item in lista)
+    {
+        Console.WriteLine($"{item} ");
+
+    }
+    Console.WriteLine($"\nTamaño={lista.Count}; "+$"Capacidad= {lista.Capacity}");
+    int eliminarLista = lista.IndexOf("Azul");
+    if (eliminarLista != -1)
+    {
+
+        Console.WriteLine($"la lista contoiene azul " + "en el indice {0}", eliminarLista);
+    }
+    else
+    {
+        Console.WriteLine($"La lista no contiene auzl");
+    }
+    
+}
